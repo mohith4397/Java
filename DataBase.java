167e3ebbe8a63b020aed61c35ac2b5b3b8189e76
Mohith Mohan <mohithmail@gmail.com>
2016-12-29 10:05:29 +0530

    Add files via upload

--- DataBase.java
+++ DataBase.java
@@ -1,22 +1,50 @@
 import java.io.*;
-public class Database{
-public static void main(String [] args)
+import java.util.Scanner;
+public class DataBase
 {
-Manager mgr=new Manager();
-Engineer engr=new Engineer();
+public static void main(String[]args)
+{
+Execution exe=new Execution();
+exe.DataManipulation();
 }
 }
 
-class Employee
+
+class Employee {String name,id,ssid;int salary;}
+class Manager extends Employee {String Department;}
+class Engineer extends Employee {String Designation;}
+class Execution {
+Scanner reader = new Scanner(System.in);
+Engineer engnr=new Engineer();
+Manager mgr=new Manager();
+void DataManipulation(){
+System.out.println("----MENU----");
+System.out.println("1.Enter Data");
+System.out.println("2.Display Data");
+System.out.println("3.Exit");
+System.out.println("4.Continue");
+int choice=reader.nextInt();
+switch (choice)
 {
-String name,id,ssid;
-long int salary;
+case 1:
+	System.out.println("Enter the name");
+	mgr.name=reader.next();
+	reak;
+case 2:
+	System.out.println("Name: " + mgr.name);
+	System.out.println("id: " + mgr.id);
+	System.out.println("SSID: " + mgr.ssid);
+	System.out.println("Salary of Manager: " + mgr.salary);
+	System.out.println("Salary of Engineer: " + engnr.salary);
+	break;
+case 3:
+	System.exit(0);
+case 4:
+	DataManipulation();
+default:
+	System.out.println("Invalid choice");
+	DataManipulation();
+	break;
 }
-class Manager extends Employee
-{
-String Department;
 }
-class Engineer extends Employee
-{
-String Designation;
 }
