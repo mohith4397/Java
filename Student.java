362fa3baac4639af6913e0b2ad55a924dd9e4716
Mohith Mohan <mohithmail@gmail.com>
2017-01-09 15:38:51 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,18 @@
+import java util.Scanner*;
+import java.io.*;
+public class Student
+{
+String Name="Mohith";
+int age=23;
+String Course="BigData";
+String Location="Koramangala";
+public static void main(String []m)
+{
+Student mohi=new Student();
+System.out.println(mohi.Name);
+System.out.println(mohi.age);
+System.out.println(mohi.Course);
+System.out.println(mohi.Location);
+}
+
+}
